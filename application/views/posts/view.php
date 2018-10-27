e5f5b3b5ece255c00da9bc3b5dc50b18c6c8a93b
zoe606 <rive99erlangga@gmail.com>
2018-10-27 08:43:57 +0700

    add comments

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -13,3 +13,30 @@
 <?php echo form_open('/posts/delete/'.$post['id']); ?>
 <input type="submit" value="delete" class="btn btn-danger">
 </form>
+<hr>
+<h3>Comments</h3>
+<?php if($comments) : ?>
+	<?php foreach($comments as $comment) : ?>
+	<div class="well">
+		<h5><?php echo $comment['body']; ?> By <strong><?php echo $comment['name']; ?></strong></h5>
+	</div>
+	<?php endforeach; ?>
+<?php endif; ?>
+<hr>
+<h3>Add Comment</h3>
+<?php echo validation_errors(); ?>
+<?php echo form_open('comments/create/'.$post['id']); ?>
+	<div class="form-group">
+		<label for="">Name</label>
+		<input type="text" name="name" class="form-control">
+	</div>
+	<div class="form-group">
+		<label for="">Emai</label>
+		<input type="text" name="email" class="form-control">
+	</div>
+	<div class="form-group">
+		<label for="">Body</label>
+		<Textarea name="body" class="form-control"> </Textarea>
+	</div>
+	<input type="hidden" name="slug" value="<?php echo $post['slug']; ?>">
+	<button class="btn btn-primary" type="submit">Submit</button>
